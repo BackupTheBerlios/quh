a11d990c4ddf1ee347762979a4fd21cb85f763d7
noisyb <noisyb>
2007-04-01 04:32:45 +0000

    *** empty log message ***

--- php_quh/htdocs/php_quh_misc.php
+++ php_quh/htdocs/php_quh_misc.php
@@ -27,38 +27,93 @@ require_once ("MP3/Id.php");
 
 
 function
-php_quh_player ($w, $php_quh)
+php_quh_nfo ($w, $search)
 {
-  $file = "";
-  $artist = "";
-  $title = "";
-  $album = "";
-  $year = "";
-  $track = 0;
-  $genre = "";
-  $length = "00:00:00";
-  $size = 0;  
+  $wikipedia_url = "http://www.wikipedia.org/wiki/";
+  $google_url = "http://www.google.com/search?q=";
+  $lyrics_url = "http://search.azlyrics.com/cgi-bin/azseek.cgi?q=";
 
-echo "|".$php_quh->incoming."|".$php_quh->incoming_abs."|";
+  return $w->widget_a ($wikipedia_url
+                      .str_replace (" ", "_", $search),
+                       "_blank",
+                       "images/wikipedia.png",
+                       -1,
+                       -1,
+                       NULL,
+                       "Search Wikipedia for \""
+                      .$search
+                      ."\"",
+                       0)
+        .$w->widget_a ($google_url
+                      .str_replace (" ", "+", $search),
+                       "_blank",
+                       "images/google.png",
+                       -1,
+                       -1,
+                       NULL,
+                       "Search Google for \""
+                      .$search
+                      ."\"",
+                       0)
+        .$w->widget_a ($lyrics_url
+                      .str_replace (" ", "+", $search),
+                       "_blank",
+                       "images/lyrics.png",
+                       -1,
+                       -1,
+                       NULL,
+                       "Search lyrics for \""
+                      .$search
+                      ."\"",
+                       0);
+}
+
+
+function
+php_quh_player ($w,
+                $php_quh_prev_file_value,
+                $php_quh_next_file_value,
+                $php_quh_file_value,
+                $php_quh_start_value,
+                $php_quh_stream_value,
+                $php_quh_pos_value)
+{
+  $php_quh_incoming = dirname ($_SERVER['PHP_SELF'])."/incoming/";
+  $php_quh_incoming_abs = dirname ($_SERVER['SCRIPT_FILENAME'])."/incoming/";
+//  $skin = dirname ($_SERVER['PHP_SELF'])."/skin/";
+//  $skin_abs = dirname ($_SERVER['SCRIPT_FILENAME'])."/skin/";
+
+echo "|".$php_quh_incoming."|".$php_quh_incoming_abs."|";
+
+
+  $php_quh_file = "";
+  $php_quh_artist = "";
+  $php_quh_title = "";
+  $php_quh_album = "";
+  $php_quh_year = "";
+  $php_quh_track = 0;
+  $php_quh_genre = "";
+  $php_quh_length = "00:00:00";
+  $php_quh_size = 0;  
 
   $p = "";
 
-  if ($php_quh->file)
+  if ($php_quh_file_value)
     {
       $id3 = &new MP3_Id ();
-      $id3->read ($php_quh->incoming_abs.basename ($php_quh->file));
-      $id3->study ($php_quh->incoming_abs.basename ($php_quh->file));
-//      $stat = stat ($php_quh->incoming_abs.basename ($php_quh->file));
+      $id3->read ($php_quh_incoming_abs.basename ($php_quh_file_value));
+      $id3->study ($php_quh_incoming_abs.basename ($php_quh_file_value));
+//      $stat = stat ($php_quh_incoming_abs.basename ($php_quh_file_value));
 
-      $file = basename ($php_quh->file);
-      $artist = $id3->getTag ('artists');
-      $title = $id3->getTag ('name');
-      $album = $id3->getTag ('album');
-      $year = $id3->getTag ('year');
-      $track = $id3->getTag ('track');
-      $genre = $id3->getTag ('genre');
-      $length = $id3->lengthh;
-      $size = $id3->musicsize;
+      $php_quh_file = basename ($php_quh_file_value);
+      $php_quh_artist = $id3->getTag ('artists');
+      $php_quh_title = $id3->getTag ('name');
+      $php_quh_album = $id3->getTag ('album');
+      $php_quh_year = $id3->getTag ('year');
+      $php_quh_track = $id3->getTag ('track');
+      $php_quh_genre = $id3->getTag ('genre');
+      $php_quh_length = $id3->lengthh;
+      $php_quh_size = $id3->musicsize;
 /*
     * version of mpeg
     var $mpeg_ver = 0;
@@ -107,28 +162,32 @@ echo "|".$php_quh->incoming."|".$php_quh->incoming_abs."|";
 */
     }
 
+
   $p .= "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">"
        ."<tr>"
        ."<td style=\"background-image:url('images/black_opaque80.png');\" valign=\"top\" width=\"400\">"
        ."<font face=\"arial,sans-serif\" size=\"-1\" color=\"00e300\">"
        ."File: "
-       .$file
+       .$php_quh_file
        ."<br>\nArtist: "
-       .$artist
+       .$php_quh_artist
+       .($php_quh_artist ? " ".php_quh_nfo ($w, $php_quh_artist) : "")
        ."<br>\nTitle: "
-       .$title
+       .$php_quh_title
+       .($php_quh_title ? " ".php_quh_nfo ($w, $php_quh_title) : "")
        ."<br>\nAlbum: "
-       .$album
+       .$php_quh_album
+       .($php_quh_album ? " ".php_quh_nfo ($w, $php_quh_album) : "")
        ."<br>\nYear: "
-       .$year
+       .$php_quh_year
        ."<br>\nTrack: "
-       .$track
+       .$php_quh_track
        ."<br>\nGenre: "
-       .$genre
+       .$php_quh_genre
        ."<br>\nLength: "
-       .$length
+       .$php_quh_length
        .", "
-       .$size
+       .$php_quh_size
        ." Bytes"
        ."</td></tr></table>";
 
@@ -136,7 +195,7 @@ echo "|".$php_quh->incoming."|".$php_quh->incoming_abs."|";
    $next =
            $_SERVER['PHP_SELF']
            ."?php_quh_file="
-           .$php_quh->incoming
+           .$php_quh_incoming
            ."/"  
            .$next_file
            ."&php_quh_start="
@@ -144,35 +203,38 @@ echo "|".$php_quh->incoming."|".$php_quh->incoming_abs."|";
            ."&php_quh_stream="
            ."1"
            ."&php_quh_pos="
-           .$php_quh->pos + 1
+           .$php_quh_pos_value + 1
 */
 
-  $p .= $w->widget_image ("php_quh_back", NULL, "images/back.png", -1, -1, "Previous Song", 0)
+  $p .= $w->widget_audio ($php_quh_file_value, $php_quh_start_value, $php_quh_stream_value, $php_quh_next_file_value)
+
+       .$w->widget_image ("php_quh_back", NULL, "images/back.png", -1, -1, "Previous Song", 0)
        .$w->widget_image ("php_quh_play", NULL, "images/play.png", -1, -1, "Play Song", 0)
        .$w->widget_image ("php_quh_pause", NULL, "images/pause.png", -1, -1, "Pause Song", 0)
        .$w->widget_image ("php_quh_stop", NULL, "images/stop.png", -1, -1, "Stop Song", 0)
        .$w->widget_image ("php_quh_next", NULL, "images/forward.png", -1, -1, "Next Song", 0)
+
 //       .$w->widget_slider ("php_quh_volume", "value", "tooltip", 0, 0)
 //       .$w->widget_slider ("php_quh_pos", "value", "tooltip", 0, 0)
+
        ."<br>"
        ."<div>";
 
-//  $next_file = "";
-echo "|".$php_quh->incoming_abs."|";
-  $dir = opendir ($php_quh->incoming_abs);
+  $next_file = "";
+  $dir = opendir ($php_quh_incoming_abs);
   for ($pos = 0; ($file = readdir ($dir)) != false; $pos++)
     if (get_suffix ($file) == ".mp3" ||
         get_suffix ($file) == ".wav")
     {
-//      $stat = stat ($php_quh->incoming_abs.$file);
-      $curr = basename ($php_quh->file) == basename ($file);
-//      if ($pos == $php_quh->pos + 1)
-//        $next_file = $file;
+      $stat = stat ($php_quh_incoming_abs.$file);
+      $curr = basename ($php_quh_file_value) == basename ($file);
+      if ($pos == $php_quh_pos_value + 1)
+        $next_file = $file;
 
-      $p.= "<a class=\"widget_a_label\" target=\"_top\" href=\""
+      $p.= "<a class=\"widget_a_label\" href=\""
           .$_SERVER['PHP_SELF']
           ."?php_quh_file="
-          .$php_quh->incoming
+          .$php_quh_incoming
           ."/"  
           .$file
           ."&php_quh_start="
@@ -196,63 +258,59 @@ echo "|".$php_quh->incoming_abs."|";
        .$w->widget_end ();
 
   return $p;
+  
 }
 
 
 function
-php_quh_upload ($w, $php_quh)
+php_quh_upload ($w)
 {
+  $php_quh_incoming = dirname ($_SERVER['PHP_SELF'])."/incoming/";
+  $php_quh_incoming_abs = dirname ($_SERVER['SCRIPT_FILENAME'])."/incoming/";
+
   $p = "<br>"
-      .$w->widget_upload ("php_quh_file", "Add", "Add a MP3 or WAV song to the playlist (max.size: 10MB)", $php_quh->incoming_abs, 10000000, "audio/mpeg", 0);
-//       .$w->widget_upload ("php_quh_skin", "Skin", "Change Skin (max.size: 250K)", $php_quh->incoming_abs, 250000, NULL, 0);
+      .$w->widget_upload ("php_quh_file", "Add", "Add a MP3 or WAV song to the playlist (max.size: 10MB)", $php_quh_incoming_abs, 10000000, "audio/mpeg", 0);
+//       .$w->widget_upload ("php_quh_skin", "Skin", "Change Skin (max.size: 250K)", $php_quh_incoming_abs, 250000, NULL, 0);
 
   return $p;
 }
 
 
 function
-php_quh_info ($w, $php_quh)
+php_quh_prefs ($w)
 {
-  $wikipedia_url = "http://www.wikipedia.org/wiki/";
-  $google_url = "http://www.google.com/search?q=";
-  $lyrics_url = "http://search.azlyrics.com/cgi-bin/azseek.cgi?q=";
+  return "<br><br>Preferences<br><br>Skin<br>"
+//        .$w->widget_checkbox ();
+//       "Winamp classic"
+//       "Default"
+;
+//proxy ($url, PROXY_FORM_FILTER);
 
-  return "<br><br>Info<br>"
+}
 
 
-  .$w->widget_a ($wikipedia_url
-                      .str_replace (" ", "_", $search),
-                       "_blank",
-                       "images/wikipedia.png",
-                       -1,
-                       -1,
-                       NULL,
-                       "Search Wikipedia for \""
-                      .$search
-                      ."\"",
-                       0)
-        .$w->widget_a ($google_url
-                      .str_replace (" ", "+", $search),
-                       "_blank",
-                       "images/google.png",
-                       -1,
-                       -1,
-                       NULL,
-                       "Search Google for \""
-                      .$search
-                      ."\"",
-                       0)
-        .$w->widget_a ($lyrics_url
-                      .str_replace (" ", "+", $search),
-                       "_blank",
-                       "images/lyrics.png",
-                       -1,
-                       -1,
-                       NULL,
-                       "Search lyrics for \""
-                      .$search
-                      ."\"",
-                       0);
+function
+php_quh_lyrics ($w)
+{
+  return "<br><br>Preferences<br><br>Skin<br>"
+//        .$w->widget_checkbox ();
+//       "Winamp classic"
+//       "Default"
+;
+//proxy ($url, PROXY_FORM_FILTER);
+
+}
+
+
+function
+php_quh_wiki ($w)
+{
+  return "<br><br>Preferences<br><br>Skin<br>"
+//        .$w->widget_checkbox ();
+//       "Winamp classic"
+//       "Default"
+;
+//proxy ($url, PROXY_FORM_FILTER);
 }
 
 
